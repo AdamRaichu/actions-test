d606ce24defcff04f44ba030f6a11ffd7656ece3
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-21 09:24:38 -0400

    added test.txt

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const content = 'Some content!';
 
-fs.writeFile('/test.txt', content, err => {
+fs.writeFile('test.txt', content, err => {
     if (err) {
         console.error(err);
     }
