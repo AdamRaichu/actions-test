191f3445afe571e9c75813d964073fe391e62702
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-25 13:40:27 -0400

    added github-actions.log

---
+++ exp_yml-log.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+fs.readFile("myFile.txt", (err, buff) => {
+  // if any error
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // otherwise log contents
+  var content = buff.toString();
+  content += "\n"
+  content += new Date()
+  content += " [INFO] exp.yml ran."
+
+  fs.writeFile('github-actions.log', content, err => {
+    if (err) {
+      console.error(err);
+    }
+    console.log("action logged")
+    // file written successfully
+  });
+});
